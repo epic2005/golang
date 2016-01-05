1c177d01aa5f38fda06ae6bffc3a18f8439c00a1
epic2005 <lipengyue@gmail.com>
2016-01-05 15:10:12 +0800

    go var test

---
+++ test/declare.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+
+    //var x int = 100;
+    var str string = "hi go"
+    var i,j,k int = 1,2,3
+    var b = true;
+    x := 200
+    const s string = "hey punk"
+    const pi float32 = 3.1415926
+
+    fmt.Println(x)
+    fmt.Println(str)
+    fmt.Println(i,j,k)
+    fmt.Println(b)
+    fmt.Printf("%d\n",x)
+    
+    fmt.Println(s)
+    fmt.Println(pi)
+
+}
