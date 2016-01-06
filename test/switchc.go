59da67a053317d767e855f3e53d0604751c5efe1
epic2005 <lipengyue@gmail.com>
2016-01-06 16:11:05 +0800

    add 3

--- test/switchc.go
+++ test/switchc.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func main(){
     //i:=1
-    var i int = 2 
+    var i int = 3 
     switch i{
         case 1:
             fmt.Println("one")
         case 2:
             fmt.Println("two")
+        case 3:
+            fmt.Println("three")
         default:
             fmt.Println("invalid value1")
     }
