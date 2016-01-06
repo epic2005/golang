8bac64b13d1364ec884a7b88ab95ed3ee660c3e5
epic2005 <lipengyue@gmail.com>
2016-01-06 19:31:50 +0800

    point test

--- test/point.go
+++ test/point.go
@@ -4,6 +4,13 @@ import "fmt"
 
 func main(){
     var i int = 1
+    var pInt *int = &i
 
-    fmt.Println(i)
+    fmt.Printf("i=%d\tpInt=%p\tpInt=%d\n",i,pInt,*pInt)
+    
+    *pInt = 2
+    fmt.Printf("i=%d\tpInt=%p\tpInt=%d\n",i,pInt,*pInt)
+    
+    i = 3
+    fmt.Printf("i=%d\tpInt=%p\tpInt=%d\n",i,pInt,*pInt)
 }
