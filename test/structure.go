cb6cf9111ab27900f2e0fd0df29e76531efaac9c
epic2005 <lipengyue@gmail.com>
2016-01-11 11:07:12 +0800

    struct test

---
+++ test/structure.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+type Person struct{
+    name string
+    age int
+    email string
+}
+
+func main(){
+    person := Person{"Tom", 30, "tom@gmail.com"}
+    person = Person{name:"Tom", age:30, email: "tom@gmail.com"}
+
+    fmt.Println(person)
+}
