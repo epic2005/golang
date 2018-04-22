1a8c77a5f9f1a1904619f4daa0e0c387c6414489
李 朋越 <lipengyue@gmail.com>
2018-04-22 12:15:10 +0800

    #slice map review diy

--- gomonkey/robot1/reference.go
+++ gomonkey/robot1/reference.go
@@ -1,23 +1,27 @@
+
 package main
 
+import (
+	"fmt"
+)
 
 func mkslice() []int {
 	s := make([]int, 0, 10)
-	s = append(s, 100)
+	s = append(s, 200)
 	return s
 }
 
 func mkmap() map[string]int {
 	m := make(map[string]int)
-	m["a"] = 300
+	m["a"] = 100
 	return m
 }
 
+
 func main() {
+	s := mkslice()
+	fmt.Printf("slice 0 is : %d\n", s[0])
 
 	m := mkmap()
-	println(m["a"])
-
-	s := mkslice()
-	println(s[0])
+	fmt.Printf("map a is: %d\n", m["a"])
 }
